de1118d973974e48e760825ab7b3d8c10727d4e4
anastasizzz <anastasizzzs10@gmail.com>
2024-07-21 23:57:35 +0300

    #3 remove hardcode strings

---
+++ android/app/src/main/java/by/eapp/musicroom/screens/view/login/LogInScreen.kt
@@ -0,0 +1,128 @@
+package by.eapp.musicroom.screens.view.login
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import by.eapp.musicroom.R
+import by.eapp.musicroom.domain.model.LoginData
+import by.eapp.musicroom.navigation.Screens
+import by.eapp.musicroom.screens.AuthorizationViewModel
+import by.eapp.musicroom.screens.LoginScreenAction
+import by.eapp.musicroom.screens.components.LogInButton
+import by.eapp.musicroom.screens.components.PasswordTextInputField
+import by.eapp.musicroom.screens.components.TextInputField
+
+@Composable
+fun LoginScreen(
+    navController: NavHostController,
+    viewModel: AuthorizationViewModel,
+) {
+    var email by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+    var showPassword by remember { mutableStateOf(false) }
+
+    val state by viewModel.stateUi.collectAsState()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Black)
+            .padding(20.dp),
+        horizontalAlignment = Alignment.Start,
+    ) {
+        Spacer(modifier = Modifier.height(70.dp))
+        Text(
+            text = stringResource(R.string.mr_sign_in),
+            fontSize = 50.sp,
+            color = Color.White,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(70.dp))
+
+        Text(
+            text = "Email",
+            modifier = Modifier.padding(bottom = 5.dp),
+            color = Color.White
+        )
+        TextInputField(
+            value = email,
+            onValueChange = { email = it },
+            placeholderText = stringResource(R.string.mr_email_placeholder)
+        )
+        Spacer(modifier = Modifier.height(40.dp))
+        Text(
+            text = stringResource(R.string.mr_password),
+            modifier = Modifier.padding(bottom = 5.dp),
+            color = Color.White
+        )
+        PasswordTextInputField(
+            password = password,
+            onPasswordChange = { password = it },
+            showPassword = showPassword,
+            onShowPasswordChange = { showPassword = it }
+        )
+        Spacer(modifier = Modifier.height(40.dp))
+        LogInButton(
+            enabled = true,
+            onClick = {
+                viewModel.dispatch(
+                    LoginScreenAction.LoginUser(
+                        LoginData(
+                            email = email,
+                            password = password
+                        )
+                    )
+                )
+            }
+        )
+
+        Spacer(modifier = Modifier.height(40.dp))
+        Text(text = stringResource(R.string.mr_don_t_have_an_account), color = Color.White)
+        Text(
+            text = stringResource(R.string.mr_sign_up), color = Color.White,
+            modifier = Modifier.clickable {
+                navController.navigate(
+                    Screens.RegistrationScreen.route
+                )
+            })
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewLogInButton() {
+    LogInButton(onClick = {})
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewTextInputField() {
+    TextInputField(
+        value = "",
+        onValueChange = {},
+        placeholderText = "placeholder"
+    )
+}
--- android/app/src/main/java/by/eapp/musicroom/screens/view/registration/RegistrationScreen.kt
+++ android/app/src/main/java/by/eapp/musicroom/screens/view/registration/RegistrationScreen.kt
@@ -1,5 +1,3 @@
-package by.eapp.musicroom.screens.login
-
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
@@ -9,7 +7,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -18,29 +15,31 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import by.eapp.musicroom.domain.model.LoginData
+import by.eapp.musicroom.R
 import by.eapp.musicroom.navigation.Screens
-import by.eapp.musicroom.screens.AuthorizationViewModel
-import by.eapp.musicroom.screens.LoginScreenAction
 import by.eapp.musicroom.screens.components.LogInButton
 import by.eapp.musicroom.screens.components.PasswordTextInputField
 import by.eapp.musicroom.screens.components.TextInputField
 
-@Composable
-fun LoginScreen(
-    navController: NavHostController,
-    viewModel: AuthorizationViewModel,
-) {
-    var email by rememberSaveable { mutableStateOf("") }
-    var password by rememberSaveable { mutableStateOf("") }
-    var showPassword by remember { mutableStateOf(false) }
 
-    val state by viewModel.stateUi.collectAsState()
+@Composable
+fun RegistrationScreen(
+    navController: NavHostController
+) {
+    var nickname by rememberSaveable { mutableStateOf("") }
+    var email by rememberSaveable { mutableStateOf("") }
+
+    var password by remember { mutableStateOf("") }
+    var repeatPassword by remember { mutableStateOf("") }
+    var showPassword by remember { mutableStateOf(false) }
+    var showRepeatPassword by remember { mutableStateOf(false) }
 
     Column(
         modifier = Modifier
@@ -51,13 +50,24 @@ fun LoginScreen(
     ) {
         Spacer(modifier = Modifier.height(70.dp))
         Text(
-            text = "Sign In",
+            text = stringResource(R.string.mr_sign_up),
             fontSize = 50.sp,
             color = Color.White,
             fontWeight = FontWeight.Bold
         )
         Spacer(modifier = Modifier.height(70.dp))
 
+        Text(
+            text = stringResource(R.string.mr_nickname),
+            modifier = Modifier.padding(bottom = 5.dp),
+            color = Color.White
+        )
+        TextInputField(
+            value = nickname,
+            onValueChange = { nickname = it },
+            placeholderText = stringResource(R.string.mr_placeholder_nickname),
+        )
+        Spacer(modifier = Modifier.height(20.dp))
         Text(
             text = "Email",
             modifier = Modifier.padding(bottom = 5.dp),
@@ -66,9 +76,10 @@ fun LoginScreen(
         TextInputField(
             value = email,
             onValueChange = { email = it },
-            placeholderText = "anastasizzzs10@gmail.com"
+            placeholderText = stringResource(R.string.mr_email_placeholder),
         )
-        Spacer(modifier = Modifier.height(40.dp))
+        //passwords
+        Spacer(modifier = Modifier.height(20.dp))
         Text(
             text = "Password",
             modifier = Modifier.padding(bottom = 5.dp),
@@ -80,46 +91,58 @@ fun LoginScreen(
             showPassword = showPassword,
             onShowPasswordChange = { showPassword = it }
         )
+
+        // Repeat password field
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(
+            text = stringResource(R.string.mr_repeat_password),
+            modifier = Modifier.padding(bottom = 5.dp),
+            color = Color.White
+        )
+        PasswordTextInputField(
+            password = repeatPassword,
+            onPasswordChange = { repeatPassword = it },
+            showPassword = showRepeatPassword,
+            onShowPasswordChange = { showRepeatPassword = it }
+        )
         Spacer(modifier = Modifier.height(40.dp))
         LogInButton(
             enabled = true,
-            onClick = {
-                viewModel.dispatch(
-                    LoginScreenAction.LoginUser(
-                        LoginData(
-                            email = email,
-                            password = password
-                        )
-                    )
-                )
-            }
+            onClick = {}
         )
 
         Spacer(modifier = Modifier.height(40.dp))
-        Text(text = "Don't have an account?", color = Color.White)
-        Text(text = "Sign Up", color = Color.White,
-            modifier = Modifier.clickable {
-                navController.navigate(
-                    Screens.RegistrationScreen.route
-                )
-            })
-
+        Text(text = stringResource(R.string.mr_already_have_an_account), color = Color.White)
+        Text(
+            text = stringResource(R.string.mr_sign_up),
+            modifier = Modifier
+                .padding(5.dp)
+                .clickable {
+                    navController.navigate(Screens.LoginScreen.route)
+                },
+            color = Color.White
+        )
     }
 }
 
+
 @Preview(showBackground = true)
 @Composable
-fun PreviewLogInButton() {
-    LogInButton(onClick = {})
+fun PasswordTextInputFieldPreview() {
+
+    var password by remember { mutableStateOf("") }
+    var showPassword by remember { mutableStateOf(false) }
+
+    PasswordTextInputField(
+        password = password,
+        onPasswordChange = { password = it },
+        showPassword = showPassword,
+        onShowPasswordChange = { showPassword = it }
+    )
 }
 
-
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun PreviewTextInputField() {
-    TextInputField(
-        value = "",
-        onValueChange = {},
-        placeholderText = "placeholder"
-    )
+fun RegistrationScreenPreview() {
+    RegistrationScreen(navController = NavHostController(context = LocalContext.current))
 }
